5b9acc92ac2b2101243758b0bb2c4a28cd5a06e7
Wahyu Wardaya <wardaya56@gmail.com>
2024-10-28 00:24:16 +0800

    menambahkan id di html

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //fungsi menghilangkan dan menampilkan form
 const form = document.getElementById('editForm');
 form.style.display = 'none';
-
 const editButton = document.getElementById('editProfileBtn');
-
 editButton.onclick = function () {
   if (form.style.display === 'none') {
     form.style.display = 'block';
